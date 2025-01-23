3705f678be62095151c54f0544241aa4c891352c
= <=>
2025-01-23 19:40:20 -0300

    Pokedex em js

--- script.js
+++ script.js
@@ -1,45 +1,61 @@
 var containerPokemon = document.getElementById("container-pokemon");
 
 var botaoAnterior = document.getElementById("anterior");
-
 var botaoProximo = document.getElementById("proximo");
 
-
 var deslocamentoAtual = 0;
-var limite = 12;
+var limite = 9;
 
-function exibirPokemon(listaPokemon){
-    containerPokemon.innerHTML= "";
+function exibirPokemon(listaPokemon) {
+    containerPokemon.innerHTML = ""; // Limpa o container
     listaPokemon.forEach((pokemon) => {
-        var nomePokemon = document.createElement("p");
-        nomePokemon.classList.add("nome-pokemon");
-        nomePokemon.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-        containerPokemon.appendChild(nomePokemon);
+        // Buscar detalhes do Pokémon para obter a imagem
+        fetch(pokemon.url)
+            .then((resposta) => resposta.json())
+            .then((detalhes) => {
+                var card = document.createElement("div");
+                card.classList.add("card-pokemon");
+
+                var nomePokemon = document.createElement("p");
+                nomePokemon.classList.add("nome-pokemon");
+                nomePokemon.textContent =
+                    pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+
+                var imagemPokemon = document.createElement("img");
+                imagemPokemon.classList.add("imagem-pokemon");
+                imagemPokemon.src = detalhes.sprites.front_default;
+                imagemPokemon.alt = pokemon.name;
+
+                card.appendChild(imagemPokemon);
+                card.appendChild(nomePokemon);
+                containerPokemon.appendChild(card);
+            });
     });
 }
 
-function buscarPokemon(deslocamento){
-    fetch(`http://pokeapi.co/api/v2/pokemon?offset=${deslocamento}&limit=${limite}`)
-    .then((resposta) => resposta.json())
-    .then((dados) => {
-        exibirPokemon(dados.results);
-        alternarBotoes(deslocamento > 0, dados.next != null);
-    });
+function buscarPokemon(deslocamento) {
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${deslocamento}&limit=${limite}`)
+        .then((resposta) => resposta.json())
+        .then((dados) => {
+            exibirPokemon(dados.results);
+            alternarBotoes(deslocamento > 0, dados.next != null);
+        });
 }
 
-function alternarBotoes(anterior, proximo){
+function alternarBotoes(anterior, proximo) {
     botaoAnterior.disabled = !anterior;
     botaoProximo.disabled = !proximo;
 }
 
-botaoAnterior.addEventListener("click", () =>{
+botaoAnterior.addEventListener("click", () => {
     deslocamentoAtual -= limite;
     buscarPokemon(deslocamentoAtual);
-})
+});
 
-botaoProximo.addEventListener("click", () =>{
+botaoProximo.addEventListener("click", () => {
     deslocamentoAtual += limite;
     buscarPokemon(deslocamentoAtual);
-})
+});
 
+// Inicializa a busca
 buscarPokemon(deslocamentoAtual);
